e7bf5ab142ded376b31da7de0e1c8a716e9c5431
Pavel <pasap@m/ru>
2024-08-01 22:34:10 +0300

    fix 0.1

--- bugtracker/tracker/models.py
+++ bugtracker/tracker/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 class Task(models.Model):
-    subject = models.CharField(max_length=255)
+    subject = models.IntegerField(unique=True, null=False)
+    annotation = models.CharField(max_length=255, null=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default='Open')
-
     def __str__(self):
-        return self.subject
+        return str(self.subject)
 
     def last_comment_date(self):
         last_comment = self.comments.order_by('-created_at').first()
@@ -18,6 +18,8 @@ class Task(models.Model):
         last_comment = self.comments.order_by('-created_at').first()
         return last_comment.body[:100] + '...' if last_comment else ''
 
+    last_comment_date.short_description = 'Date of Last Comment'
+    last_comment_date.admin_order_field = 'comments__created_at'
 
 class Comment(models.Model):
     task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
--- bugtracker/tracker/views.py
+++ bugtracker/tracker/views.py
@@ -5,17 +5,26 @@ from django.core.paginator import Paginator
 
 def task_list(request):
     query = request.GET.get('q')
-    if query:
-        tasks = Task.objects.filter(id=query)
-    else:
-        tasks = Task.objects.all()
+    sort_by = request.GET.get('sort_by', 'created_at')
+    sort_order = request.GET.get('sort_order', 'asc')
+    order_prefix = '' if sort_order == 'asc' else '-'
 
-    paginator = Paginator(tasks, 20)  # Ограничение до 20 заявок на странице
+    tasks = Task.objects.all()
+
+    if query:
+        tasks = tasks.filter(id=query)
+
+    tasks = tasks.order_by(f'{order_prefix}{sort_by}')
+
+    paginator = Paginator(tasks, 20)
     page = request.GET.get('page')
     tasks_page = paginator.get_page(page)
 
-    return render(request, 'tracker/task_list.html', {'tasks': tasks_page})
-
+    return render(request, 'tracker/task_list.html', {
+        'tasks': tasks_page,
+        'sort_by': sort_by,
+        'sort_order': sort_order,
+    })
 
 def task_detail(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
